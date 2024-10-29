710c65e36516980345dcd21d949daa4305d06d86
marialaurashendacruz <132945567+marialaurashendacruz@users.noreply.github.com>
2024-10-29 13:46:20 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -4,14 +4,21 @@ criaCartao(
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )  
+
 criaCartao(
  'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )  
+
 criaCartao(
-    
     'Programação',
     'O que é uma função',
     'Uma função é um bloco de código que executa alguma tarefa'
 )           
+
+criaCartao(
+    'Lingua inglesa',
+    'como se diz oi em inglês',
+    'Oi em inglês é HI(RAI)'
+)
