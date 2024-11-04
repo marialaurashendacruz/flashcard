32884bee84bea67230d2f7f246854c2077888a40
marialaurashendacruz <132945567+marialaurashendacruz@users.noreply.github.com>
2024-11-04 13:49:28 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-         <div class="cartao__conteudo"> 
-            <h3>${categoria}</h3>
-         <div class="cartao__conteudo__pergunta">
-          <p> ${pergunta}</p>
-         </div>   
-         <div class="cartao__conteudo__resposta">
-           <p> ${resposta}</p>
-         </div>
+       <div class="cartao__conteudo"> 
+       <h3>${categoria}</h3>
+       <div class="cartao__conteudo__pergunta">
+       <p>${pergunta}</p>
+       </div>   
+       <div class="cartao__conteudo__resposta">
+       <p>${resposta}</p>
+       </div>
     `
-    
-    
-    let respostaEstaVisivel = false
 
-    function viarCartao(){
-      respostaEstaVisivel = !respostaEstaVisivel
-    }
-    
+
+      let respostaEstaVisivel = false
+
+      function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+      }
+      cartao.addEventListener('click', viraCartao)
+
+
     container.appendChild(cartao)
-
 
 }
--- perguntas.js
+++ perguntas.js
@@ -4,21 +4,19 @@ criaCartao(
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )  
-
 criaCartao(
  'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )  
-
 criaCartao(
+    
     'Programação',
     'O que é uma função',
     'Uma função é um bloco de código que executa alguma tarefa'
 )           
-
 criaCartao(
-    'Lingua inglesa',
-    'como se diz oi em inglês',
-    'Oi em inglês é HI(RAI)'
-)
+    'Lingua Inglesa',
+    'Como se diz oi em inglễs?',
+    'Se diz hi (RAI)'
+ )  
