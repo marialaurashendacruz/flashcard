b318e364866b69e485e11ab87ba02b9049eac8d3
marialaurashendacruz <132945567+marialaurashendacruz@users.noreply.github.com>
2024-11-12 13:46:54 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,32 @@
 
 criaCartao(
     'Biotecnologia',
-    'O que é a biotecnologia?',
+    'O que é a Biotecnologia?',
     'A biotecnologia utiliza células vivas para desenvolver ou manipular produtos com fins específicos, como, por exemplo, os alimentos transgênicos.'
 )  
 criaCartao(
- 'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+   'Biotecnologia',
+    'Qual a função da Biotecnologia?',
+    'Sua função é melhorar geneticamente os produtos para que haja mais quantidade ou melhor qualidade de alimentos.'
 )  
 criaCartao(
     
-    'Programação',
-    'O que é uma função',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Biotecnologia',
+    'Em quais áreas a Biotecnologia é aplicada?',
+    'A Biotecnologia é usada para desenvolver produtos e soluções em áreas como medicina, agricultura, pecuária, alimentação, bioenergia, indústria química, eletrônica, dentre outras.'
 )           
 criaCartao(
-    'Lingua Inglesa',
-    'Como se diz oi em inglễs?',
-    'Se diz hi (RAI)'
+    'Robótica',
+    'O que é a Robótica?',
+    'A Robótica é um ramo educacional e tecnológico que trata de sistemas compostos por partes mecânicas automáticas em conjunto com circuitos integrados, tornando sistemas mecânicos motorizados controlados por circuitos elétricos e inteligência computacional.'
  )  
+ criaCartao(
+    'Robótica',
+    'Qual a função da Robótica?',
+    'A robótica é responsável por explorar, criar e desenvolver novas tecnologias em máquinas, computadores, softwares e sistemas em geral, a partir do controle mecânico e automático.'
+ ) 
+ criaCartao(
+    'Robótica',
+    'Quais as áreas que a Robótica se aplica?',
+    'A robótica pode ser aplicada em várias áreas de trabalho, como indústria ou engenharias, mas também no ambiente escolar, como aprendemos em aulas.'
+ )
